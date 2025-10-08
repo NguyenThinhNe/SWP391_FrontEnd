4774f83791f67ecb2cda14af3f2d3b6eeff2a13d
Trương Minh Nhật <fleeforezz@gmail.com>
2025-10-08 22:09:03 +0700

    Create sidebar for SC Technician

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Create side bar</h1>
+      <h1>Code của ánh</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
