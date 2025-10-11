07fe5aa45d8c2974d07543de833cfe0dde2a1e5b
ThinhNguyenrt <171249327+ThinhNguyenrt@users.noreply.github.com>
2025-10-11 11:36:14 +0700

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Sidebar</h1>
+      <a> Hi, my name is Thinh</a> 
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
